2a1855c6a5b8162bb8274e16dbb676c3528e4528
CaroDtoc <olgadonkeng@gmail.com>
2026-05-13 14:03:51 +0200

    chore: mise à jour de la version vers v0.2.0

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 // AfriCode Hub - Point d'entrée de l'application 
 console.log('AfriCode Hub v0.1.0 - Démarrage...');
+alert('Bienvenue sur AfriCode Hub ! Explorez, apprenez et partagez vos connaissances en programmation avec la communauté africaine.');
